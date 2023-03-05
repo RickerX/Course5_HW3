dbf302d7aaa6a1344eb7a0925c8f7f580e361e25
roman <roman.pshenichnikov555@gmail.com>
2023-03-05 15:48:06 +0300

    Course5_HW done

---
+++ src/main/java/EmployeeDAO.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public interface EmployeeDAO {
+    // добавление объекта
+    void create(Employee employee);
+
+    // получение объекта по id
+    Employee readById(int id);
+
+    // получение списка всех объектов
+    List<Employee> readAll();
+
+    // изменение объекта по id
+    void updateEmployeeById(int id, String lastName, int city);
+
+    // удаление оюъекта по id
+    void deleteById(int id);
+}
